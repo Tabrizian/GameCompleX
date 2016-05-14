a7812286e62f3617c719183a3198a85e6c26f347
Iman Tabrizian <tabrizian@outlook.com>
2016-05-15 00:37:13 +0430

    Improves table graphics.

--- public/process.php
+++ public/process.php
@@ -9,27 +9,35 @@
 require_once('../includes/database.php');
 if(isset($_POST['sql'])) {
     $data_set = $db->query($_POST['sql']);
-    $attribute_list = mysqli_fetch_fields($data_set);
+    if(!($data_set instanceof bool)) {
+        $attribute_list = mysqli_fetch_fields($data_set);
 
-    if($attribute_list) {
-        echo "<div class=\"row\">";
-        foreach($attribute_list as $val) {
-            echo "<div class=\"col-xs-1\" style=\"background-color: #C0C0C0;\">";
-            echo $val->name;
-            echo "</div>";
-        }
-        echo "</div>";
-    }
-    if(gettype($data_set) != "boolean")
-        while($row = $db->fetch_assoc($data_set)) {
+        print_r($attribute_list);
+
+        if($attribute_list) {
             echo "<div class=\"row\">";
-            foreach($row as $item=>$value) {
-                echo "<div class=\"col-xs-1\">";
-                echo $value;
+            foreach($attribute_list as $val) {
+                echo "<div class=\"col-xs-1\" style=\"background-color: #C0C0C0;\">";
+                echo $val->name;
                 echo "</div>";
             }
             echo "</div>";
         }
+        if(gettype($data_set) != "boolean")
+            while($row = $db->fetch_assoc($data_set)) {
+                echo "<div class=\"row\">";
+                foreach($row as $item=>$value) {
+                    echo "<div class=\"col-xs-1\">";
+                    echo $value;
+                    echo "</div>";
+                }
+                echo "</div>";
+            }
+    } else {
+        if($data_set){
+            echo "Success!";
+        }
+    }
 } else {
     echo 'Wrong page';
 }
